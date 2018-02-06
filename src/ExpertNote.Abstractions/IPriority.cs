6360147d7ee7d446683fd3dab79fa822690c37b3
Malachi Burke <malachi.burke@gmail.com>
2018-02-05 21:29:01 -0800

    Initial boilerplate-ish checkin

---
+++ src/ExpertNote.Abstractions/IPriority.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpertNote.Abstractions
+{
+    /// <summary>
+    /// The combination of these elements result in an overall priority
+    /// </summary>
+    public interface IPriority
+    {
+        /// <summary>
+        /// How important this is, in the long term.  
+        /// </summary>
+        int Importance { get; }
+
+        /// <summary>
+        /// How urgent this is, in the short term
+        /// </summary>
+        int Urgency { get; }
+
+        /// <summary>
+        /// How far we've progressed into our task
+        /// </summary>
+        int Stage { get; }
+
+        DateTime? Due { get; }
+    }
+}
